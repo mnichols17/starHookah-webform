1b03eaf312ba8f0004ba5f9cfbd85f560aab0b94
Mike <mike.nichols97@gmail.com>
2020-06-27 19:47:42 -0400

    Firebase connection with firebase-admin now

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const path = require('path');
 const firebase = require('firebase');
+const admin = require('firebase-admin');
 require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const serviceAccount = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URL,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT,
+    "client_x509_cert_url": process.env.CLIENT_CERT_URL
+}
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
 
 app.use(express.json());
 
@@ -38,12 +42,10 @@ function checkUID(req, res, next) {
 }
 
 app.get('/api/order/:uid', checkUID, (req, res) => {
-    
     db.collection('stock')
     .get()
     .then(querySnapshot => {
-        const data = querySnapshot.docs.map(doc => doc.data());
-        res.json(data);
+        res.json(querySnapshot.docs.map(doc => doc.data()));
     })
 })
 
@@ -51,14 +53,9 @@ app.post('/api/order/:uid', checkUID, (req, res) => {
     const {cart} = req.body;
     const order = req.order;
 
-    try {
-        order.orderCart = [...order.orderCart, ...cart];
-        db.collection("orders").doc(order.id).set(order);
-        res.status(201).json({msg: "ORDER ADDED"});
-    }
-    catch {
-        res.status(400).json({Error: "Invalid UID"});
-    }
+    order.orderCart = [...order.orderCart, ...cart];
+    db.collection("orders").doc(order.id).set(order);
+    res.status(201).json({msg: "ORDER ADDED"});
 })
 
 app.listen(port, () => console.log(`Server running on ${port}`));
