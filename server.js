42b5fd15bca5adff0ac821503f3b75a0853689ae
Mike <mike.nichols97@gmail.com>
2020-06-28 09:08:00 -0400

    Heroku deploy

--- server.js
+++ server.js
@@ -81,4 +81,12 @@ app.post('/api/order/:uid', checkUID, (req, res) => {
     res.status(201).json({msg: "ORDER ADDED"});
 })
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.listen(port, () => console.log(`Server running on ${port}`));
